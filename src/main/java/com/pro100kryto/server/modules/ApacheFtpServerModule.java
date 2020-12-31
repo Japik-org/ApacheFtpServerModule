2c8df27777f8455a930435c0461d0314829331a9
pro100kryto <pro100kryto.original@gmail.com>
2020-12-30 23:48:32 -0300

    Create project

---
+++ src/main/java/com/pro100kryto/server/modules/ApacheFtpServerModule.java
@@ -0,0 +1,27 @@
+package com.pro100kryto.server.modules;
+
+import com.pro100kryto.server.module.Module;
+import com.pro100kryto.server.service.IServiceControl;
+
+public class ApacheFtpServerModule extends Module {
+
+
+    public ApacheFtpServerModule(IServiceControl service, String name) {
+        super(service, name);
+    }
+
+    @Override
+    public void tick() throws Throwable {
+
+    }
+
+    @Override
+    protected void startAction() throws Throwable {
+
+    }
+
+    @Override
+    protected void stopAction(boolean force) throws Throwable {
+
+    }
+}
